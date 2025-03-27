0ebaa6e41bfec5d3afeb5c0981f998b689765631
anicia smith <anicias@yahoo>
2025-03-27 18:31:08 -0400

    Router navigation component

---
+++ CodeCraftersApp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Components/Navigation/navigation'; // Make sure the path is correct
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      {/* Other components or content */}
+    </div>
+  );
+}
+
+export default App;
